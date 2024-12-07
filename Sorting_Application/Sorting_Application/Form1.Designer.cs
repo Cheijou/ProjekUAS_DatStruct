dcaf43d9ba6721386fce60d2413f84a9b0966e18
Nornyr <ignatiusfelixc@gmail.com>
2024-12-07 20:14:27 +0700

    Bikin Form

--- Sorting_Application/Sorting_Application/Form1.Designer.cs
+++ Sorting_Application/Sorting_Application/Form1.Designer.cs
@@ -28,13 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonBubble = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButtonInsert = new System.Windows.Forms.RadioButton();
+            this.radioButtonSelect = new System.Windows.Forms.RadioButton();
+            this.radioButtonRadix = new System.Windows.Forms.RadioButton();
+            this.radioButtonQuick = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelTimePassed = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-3, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(715, 94);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(237, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sorting Application";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number :";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(84, 141);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(193, 22);
+            this.textBoxInput.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonQuick);
+            this.groupBox1.Controls.Add(this.radioButtonRadix);
+            this.groupBox1.Controls.Add(this.radioButtonSelect);
+            this.groupBox1.Controls.Add(this.radioButtonInsert);
+            this.groupBox1.Controls.Add(this.radioButtonBubble);
+            this.groupBox1.Location = new System.Drawing.Point(429, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 175);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sorting Operation";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButtonBubble
+            // 
+            this.radioButtonBubble.AutoSize = true;
+            this.radioButtonBubble.Location = new System.Drawing.Point(21, 35);
+            this.radioButtonBubble.Name = "radioButtonBubble";
+            this.radioButtonBubble.Size = new System.Drawing.Size(98, 20);
+            this.radioButtonBubble.TabIndex = 0;
+            this.radioButtonBubble.TabStop = true;
+            this.radioButtonBubble.Text = "Bubble Sort";
+            this.radioButtonBubble.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Add numbers to be sort";
+            // 
+            // radioButtonInsert
+            // 
+            this.radioButtonInsert.AutoSize = true;
+            this.radioButtonInsert.Location = new System.Drawing.Point(21, 61);
+            this.radioButtonInsert.Name = "radioButtonInsert";
+            this.radioButtonInsert.Size = new System.Drawing.Size(87, 20);
+            this.radioButtonInsert.TabIndex = 1;
+            this.radioButtonInsert.TabStop = true;
+            this.radioButtonInsert.Text = "Insert Sort";
+            this.radioButtonInsert.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSelect
+            // 
+            this.radioButtonSelect.AutoSize = true;
+            this.radioButtonSelect.Location = new System.Drawing.Point(21, 87);
+            this.radioButtonSelect.Name = "radioButtonSelect";
+            this.radioButtonSelect.Size = new System.Drawing.Size(111, 20);
+            this.radioButtonSelect.TabIndex = 2;
+            this.radioButtonSelect.TabStop = true;
+            this.radioButtonSelect.Text = "Selection Sort";
+            this.radioButtonSelect.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRadix
+            // 
+            this.radioButtonRadix.AutoSize = true;
+            this.radioButtonRadix.Location = new System.Drawing.Point(21, 113);
+            this.radioButtonRadix.Name = "radioButtonRadix";
+            this.radioButtonRadix.Size = new System.Drawing.Size(90, 20);
+            this.radioButtonRadix.TabIndex = 3;
+            this.radioButtonRadix.TabStop = true;
+            this.radioButtonRadix.Text = "Radix Sort";
+            this.radioButtonRadix.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonQuick
+            // 
+            this.radioButtonQuick.AutoSize = true;
+            this.radioButtonQuick.Location = new System.Drawing.Point(21, 139);
+            this.radioButtonQuick.Name = "radioButtonQuick";
+            this.radioButtonQuick.Size = new System.Drawing.Size(89, 20);
+            this.radioButtonQuick.TabIndex = 4;
+            this.radioButtonQuick.TabStop = true;
+            this.radioButtonQuick.Text = "Quick Sort";
+            this.radioButtonQuick.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Result :";
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 16;
+            this.listBoxResult.Location = new System.Drawing.Point(12, 206);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(403, 180);
+            this.listBoxResult.TabIndex = 6;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(567, 331);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(107, 23);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(430, 331);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(107, 23);
+            this.buttonGo.TabIndex = 8;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(429, 360);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(108, 23);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // labelTimePassed
+            // 
+            this.labelTimePassed.AutoSize = true;
+            this.labelTimePassed.Location = new System.Drawing.Point(605, 301);
+            this.labelTimePassed.Name = "labelTimePassed";
+            this.labelTimePassed.Size = new System.Drawing.Size(69, 16);
+            this.labelTimePassed.TabIndex = 10;
+            this.labelTimePassed.Text = "mm-ss-ms";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(427, 301);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Executed Time Calculation :";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(567, 360);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(107, 23);
+            this.buttonStop.TabIndex = 12;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(712, 405);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labelTimePassed);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonBubble;
+        private System.Windows.Forms.RadioButton radioButtonQuick;
+        private System.Windows.Forms.RadioButton radioButtonRadix;
+        private System.Windows.Forms.RadioButton radioButtonSelect;
+        private System.Windows.Forms.RadioButton radioButtonInsert;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelTimePassed;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonStop;
     }
 }
 
--- Sorting_Application/Sorting_Application/Form1.cs
+++ Sorting_Application/Sorting_Application/Form1.cs
@@ -16,5 +16,15 @@ namespace Sorting_Application
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
