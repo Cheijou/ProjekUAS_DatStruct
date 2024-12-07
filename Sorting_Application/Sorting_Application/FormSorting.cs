8e41342dada2148afe8d2a7c948aaeb2f7af18bb
Nornyr <ignatiusfelixc@gmail.com>
2024-12-08 01:19:20 +0700

    try catch

--- Sorting_Application/Sorting_Application/FormSorting.cs
+++ Sorting_Application/Sorting_Application/FormSorting.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,36 +56,44 @@ namespace Sorting_Application
 
         private void buttonGo_Click(object sender, EventArgs e)
         {
-            listBoxResult.Items.Clear();
-            banyaknyaData = int.Parse(textBoxBanyakData.Text);
-            arrayData = new Sort(banyaknyaData);
-            for (int i = 0; i < arrayData.ArrData.Length; i++)
+            try
             {
-                arrayData.ArrData[i] = random.Next(banyaknyaData * 2);
+                listBoxResult.Items.Clear();
+                banyaknyaData = int.Parse(textBoxBanyakData.Text);
+                arrayData = new Sort(banyaknyaData);
+                for (int i = 0; i < arrayData.ArrData.Length; i++)
+                {
+                    arrayData.ArrData[i] = random.Next(banyaknyaData * 2);
+                }
+                if (radioButtonBubble.Checked == true)
+                {
+                    durasiSorting = arrayData.BubbleSort();
+                }
+                else if (radioButtonInsertion.Checked == true)
+                {
+                    durasiSorting = arrayData.InsertionSort();
+                }
+                else if (radioButtonSelection.Checked == true)
+                {
+                    durasiSorting = arrayData.SelectionSort();
+                }
+                else if (radioButtonQuick.Checked == true)
+                {
+                    int first = 0;
+                    int last = arrayData.ArrData.Length - 1;
+                    durasiSorting = arrayData.QuickSort(first, last);
+                }
+                labelDurasiSorting.Text = durasiSorting.ToString("mm' : 'ss' : 'fff");
+                for (int i = 0; i < arrayData.ArrData.Length; i++)
+                {
+                    listBoxResult.Items.Add(arrayData.ArrData[i]);
+                }
             }
-            if (radioButtonBubble.Checked == true)
+            catch(Exception ex)
             {
-                durasiSorting = arrayData.BubbleSort();
+                MessageBox.Show("isi data dengan benar");
             }
-            else if (radioButtonInsertion.Checked == true)
-            {
-                durasiSorting = arrayData.InsertionSort();
-            }
-            else if (radioButtonSelection.Checked == true)
-            {
-                durasiSorting = arrayData.SelectionSort();
-            }
-            else if (radioButtonQuick.Checked == true)
-            {
-                int first = 0;
-                int last = arrayData.ArrData.Length - 1;
-                durasiSorting = arrayData.QuickSort(first, last);
-            }
-            labelDurasiSorting.Text = durasiSorting.ToString("mm' : 'ss' : 'fff");
-            for (int i = 0; i < arrayData.ArrData.Length; i++)
-            {
-                listBoxResult.Items.Add(arrayData.ArrData[i]);
-            }
+            
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
