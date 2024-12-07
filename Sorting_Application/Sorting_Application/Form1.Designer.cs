17a8e83b8bbf139d267dce9a26de843617fa8113
Nornyr <ignatiusfelixc@gmail.com>
2024-12-07 20:30:07 +0700

    form

--- Sorting_Application/Sorting_Application/Form1.Designer.cs
+++ Sorting_Application/Sorting_Application/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(-3, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(821, 94);
+            this.panel1.Size = new System.Drawing.Size(829, 94);
             this.panel1.TabIndex = 0;
             // 
             // label1
